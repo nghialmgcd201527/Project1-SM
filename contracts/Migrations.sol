dd981e8f7ad155c3b2d434ea1f8c163dc85d4d8b
lmnghia911 <lmnghia911@gmail.com>
2022-09-23 15:18:57 +0700

    first commit

---
+++ contracts/Migrations.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Migrations {
+    address public owner;
+    uint256 public last_completed_migration; // solhint-disable-line var-name-mixedcase
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address newAddress) public restricted {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(last_completed_migration);
+    }
+}
