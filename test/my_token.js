119942c2349be2598e7fb6e3c5395e2b77815894
lmnghia911 <lmnghia911@gmail.com>
2022-10-11 19:43:51 +0700

    test

--- test/my_token.js
+++ test/my_token.js
@@ -20,7 +20,7 @@ contract("MyToken", function (accounts) {
     return assert.equal(totalSupply, 40e6 * 1e18);
   })
 
-  it("should transfer 0.1% total supply to account 1", async function () {
+  it("should transfer 0.1% total supply to account 1 at TGE", async function () {
     const instance = await myToken.deployed();
     await instance.transfer(accounts[1], 40e6 * 1e18 * 0.001);
 
@@ -29,5 +29,15 @@ contract("MyToken", function (accounts) {
 
     assert.equal(balanceAccount0.toNumber(), 40e6 * 1e18 * 0.999);
     assert.equal(balanceAccount1.toNumber(), 40e6 * 1e18 * 0.001);
+  })
+
+  it("check the balance after releasing by owner", async function () {
+    const token = await MyToken.deployed();
+    let owner = accounts[0];
+    let balanceBeforeRelease = await token.balance();
+    await token.release();
+    let balanceAfterRelease = await token.balance();
+    assert.equal(balanceBeforeRelease.toNumber(), owner.balance.toNumber());
+    assert.equal(balanceAfterRelease.toNumber(), 0);
   })
 });
