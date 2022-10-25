8c1f8ef08e1bf3659d23d720fb8aefa296533352
lmnghia911 <lmnghia911@gmail.com>
2022-10-25 19:56:39 +0700

    phan tu uyen nhi

--- test/my_token.js
+++ test/my_token.js
@@ -1,6 +1,14 @@
 const { assert } = require("console");
 const { it } = require("node:test");
 
+const { time } = require("@openzeppelin/test-helpers");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
+const bnChai = require('bn-chai');
+const expect = require('chai')
+  .use(bnChai(web3.utils.BN))
+  .expect;
+
 const myToken = artifacts.require("MyToken");
 
 /*
@@ -9,6 +17,8 @@ const myToken = artifacts.require("MyToken");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("MyToken", function (accounts) {
+  const remainingAmount = web3.utils.toWei(web3.utils.toBN(40e6 * 1e18 * 0.02));
+  let releasePeriod = web3.utils.toBN(30 * 24 * 60 * 60);
   it("should assert true", async function () {
     await myToken.deployed();
     return assert.isTrue(true);
@@ -40,4 +50,39 @@ contract("MyToken", function (accounts) {
     assert.equal(balanceBeforeRelease.toNumber(), owner.balance.toNumber());
     assert.equal(balanceAfterRelease.toNumber(), 0);
   })
+
+  it('checking balance', async () => {
+    const token = await MyToken.deployed();
+    let owner = accounts[0];
+
+    await token.transfer(accounts[1].address, remainingAmount, { from: owner });
+
+    const remaining = await account[1].remainingAmount.call();
+    expect(remaining).to.eq.BN(remainingAmount);
+
+  });
+
+  it('release', async () => {
+    const token = await MyToken.deployed();
+    let owner = accounts[0];
+
+    await time.increase(releasePeriod);
+
+    let count = 0;
+    let balance = await token.balance.call();
+    while (balance > 0) {
+      count++;
+
+      beforeBalance = await token.balanceOf.call(owner);
+      await token.release({ from: owner });
+      afterBalance = await token.balanceOf.call(owner);
+
+      await time.increase(releasePeriod);
+      balance = await token.balance.call();
+      console.log(count, balance.toString(), afterBalance.sub(beforeBalance).toString());
+    }
+
+    balance = await token.balance.call();
+    expect(balance).to.eq.BN(web3.utils.toBN(0));
+  });
 });
